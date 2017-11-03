ca69fe5ff3198938de7ecd4156ac373bfb483f44
James Pederson <james@jpederson.com>
2017-11-03 04:44:41 -0500

    Remove old analytics code, and add in the new one for WHC.

--- footer.php
+++ footer.php
@@ -36,5 +36,15 @@
 </div><!-- #container -->
 
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109193529-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109193529-1');
+</script>
+
 </body>
 </html>
