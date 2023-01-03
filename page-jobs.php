863f290bbe2e06c1b498725d113ee8a3400f2d83
James Pederson <james@jpederson.com>
2023-01-03 13:18:58 -0600

    Remove title and content from jobs template.

--- page-jobs.php
+++ page-jobs.php
@@ -6,24 +6,7 @@ Template Name: Jobs
 
 get_header();
 
-?>
-
-	<?php the_showcase(); ?>
-	
-	<div class="content-wide" role="main">
-		<?php 
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post(); 
-				?>
-			<h1 class="post-title"><?php the_title(); ?></h1>
-				<?php
-				the_content();
-			endwhile;
-		endif;
-		?>
-	</div><!-- #content -->
-
-<?php
+the_showcase();
 
 print do_shortcode( '[oscp]' );
 
